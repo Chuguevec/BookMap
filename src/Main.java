03f094ab96090edfafb72b35313ddeb24b334cd2
Vladimir Chuguevec <777helk@mail.ru>
2022-09-20 20:02:51 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,126 +1,175 @@
 import java.io.*;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Optional;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
-    static HashMap<Integer, Integer> bid = new HashMap<>();
-    static HashMap<Integer, Integer> ask = new HashMap<>();
-    static HashMap<Integer, Integer> spread = new HashMap<>();
+    TreeMap<Integer, Integer> bid = new TreeMap<>();
+    TreeMap<Integer, Integer> ask = new TreeMap<>();
+    private final StringBuilder data = new StringBuilder();
 
 
     public static void main(String[] args) {
-        createOutputFile();
-        readData();
+        Main main = new Main();
+        main.readData();
+        main.writeData(main.data.toString());
 
     }
-
-    public static void buy(int size) {
-        int keyMinPrice = bestPriceAsk();
-        int currentSize = ask.get(keyMinPrice);
-        int resultSize = currentSize - size;
-        if (resultSize > 0) {
-            ask.put(keyMinPrice, resultSize);
-        } else {
-            ask.remove(keyMinPrice);
-        }
-    }
-
-    public static void sell(int size) {
-        int keyMaxPrice = bestPriceBid();
-        int currentSize = bid.get(keyMaxPrice);
-        int resultSize = currentSize - size;
-        if (resultSize > 0) {
-            bid.put(keyMaxPrice, resultSize);
-        } else {
-            bid.remove(keyMaxPrice);
-        }
-    }
-
-    public static void readData() {
+    public void readData() {
 
         try (FileReader fr = new FileReader("input.txt");
              BufferedReader bufferedReader = new BufferedReader(fr)) {
             while (bufferedReader.ready()) {
                 String s = bufferedReader.readLine();
-                String[] array = s.split(",");
+                String[] array = split(s);
                 String prefix = array[0];
                 switch (prefix) {
                     case ("u") -> updateData(array);
                     case ("q") -> printData(array);
                     case ("o") -> doOrder(array);
-                    default -> System.out.println("Wrong data in input.txt");
                 }
-
             }
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void updateData(String[] s) {
+    private void updateData(String[] s) {
         String end = s[3];
         int keyPrice = Integer.parseInt(s[1]);
         int valueSize = Integer.parseInt(s[2]);
         switch (end) {
-            case ("bid") -> bid.put(keyPrice, valueSize);
-            case ("ask") -> ask.put(keyPrice, valueSize);
-            default -> System.out.println("Error, wrong data!");
+            case ("bid") -> setBid(keyPrice, valueSize);
+            case ("ask") -> setAsk(keyPrice, valueSize);
+        }
+    }
+    private void setBid(int key, int value) {
+        if (value > 0) {
+            bid.put(key, value);
+        } else {
+            bid.remove(key);
         }
     }
 
-    private static void printData(String[] s) {
-        switch (s[1]) {
-            case ("best_bid") -> writeData(bestPriceBid() + "," + bid.get(bestPriceBid()) + "\n");
-            case ("best_ask") -> writeData(bestPriceAsk() + "," + ask.get(bestPriceAsk()));
-            case ("size") -> writeData(getSizeSpecifiedPrice(Integer.parseInt(s[2])) + "\n");
-            default -> System.out.println("Error, wrong data!");
+    private void setAsk(int key, int value) {
+        if (value > 0) {
+            ask.put(key, value);
+        } else {
+            ask.remove(key);
         }
     }
 
-    private static void doOrder(String[] s) {
+    private void doOrder(String[] s) {
         int size = Integer.parseInt(s[2]);
         switch (s[1]) {
             case ("buy") -> buy(size);
             case ("sell") -> sell(size);
-            default -> System.out.println("Error, wrong data!");
         }
     }
 
-    private static int bestPriceAsk() {
-        Optional<Integer> minValue = ask.keySet().stream().min(Comparator.comparingInt(n -> n));
-        return minValue.orElse(-1);
-
-    }
-
-    private static int bestPriceBid() {
-        Optional<Integer> keyMaxPrice = bid.keySet().stream().max(Comparator.comparingInt(n -> n));
-        return keyMaxPrice.orElse(-1);
-    }
-
-    private static void writeData(String s) {
-        try (FileWriter fileWriter = new FileWriter("output.txt", true)) {
-            fileWriter.write(s);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    private void buy(int size) {
+        while (size > 0) {
+            int[] keyValue = getBestPriceAsk();
+            int keyMinPrice = keyValue[0];
+            int currentSize = keyValue[1];
+            int resultSize = currentSize - size;
+            if (resultSize > 0) {
+                ask.put(keyMinPrice, resultSize);
+                return;
+            } else if (resultSize < 0) {
+                ask.remove(keyMinPrice);
+                size = size - currentSize;
+            } else {
+                ask.remove(keyMinPrice);
+                return;
+            }
         }
     }
 
-    private static int getSizeSpecifiedPrice(int price) {
+    private void sell(int size) {
+        while (size > 0) {
+            int[] keyValue = getBestPriceBid();
+            int keyMaxPrice = keyValue[0];
+            int currentSize = keyValue [1];
+            int resultSize = currentSize - size;
+            if (resultSize > 0) {
+                bid.put(keyMaxPrice, resultSize);
+                return;
+            } else if (resultSize < 0) {
+                bid.remove(keyMaxPrice);
+                size = size -currentSize;
+            } else {
+                bid.remove(keyMaxPrice);
+                return;
+            }
+        }
+    }
+
+    private void printData(String[] s) {
+        switch (s[1]) {
+            case ("best_bid") -> {
+                int [] keyValue = getBestPriceBid();
+                if ( keyValue [0] != -1){
+                    data.append(keyValue [0]).append(",").append(keyValue[1]).append('\n');
+                }
+            }
+            case ("best_ask") -> {
+                int [] keyValue = getBestPriceAsk();
+                if (keyValue[0] != -1){
+                    data.append(keyValue[0]).append(',').append(keyValue[1]).append('\n');
+                }
+            }
+            case ("size") -> data.append(getSizeSpecifiedPrice(Integer.parseInt(s[2]))).append('\n');
+        }
+    }
+
+    private int[] getBestPriceAsk() {
+        Map.Entry<Integer, Integer> entry = ask.firstEntry();
+        if (entry != null){
+            int[] array = new int[2];
+            array[0] = entry.getKey();
+            array[1] = entry.getValue();
+            return array;
+        } else return new int[]{-1, -1};
+
+    }
+
+    private int[] getBestPriceBid() {
+        Map.Entry<Integer, Integer> entry = bid.lastEntry();
+        if (entry != null){
+            int[] array = new int[2];
+            array[0] = entry.getKey();
+            array[1] = entry.getValue();
+            return array;
+        }else return new int[] {-1,-1};
+    }
+
+    private void writeData(String data) {
+            try (FileWriter fileWriter = new FileWriter("output.txt", true)) {
+                fileWriter.write(data);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+    }
+
+    private int getSizeSpecifiedPrice(int price) {
         if (bid.containsKey(price)) return bid.get(price);
-        else if (ask.containsKey(price)) return ask.get(price);
-        else return spread.getOrDefault(price, -1);
+        else return ask.getOrDefault(price, 0);
     }
 
-    private static void createOutputFile (){
-        File file = new File("output.txt");
-        try {
-            file.delete();
-            file.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    private String [] split (String s){
+        String [] splitData = new String[4];
+        int count = 0;
+        int beginIndex = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ','){
+                splitData [count] = s.substring(beginIndex, i);
+                beginIndex = i+1;
+                count++;
+            }
         }
+        splitData [count] = s.substring(beginIndex);
+        return splitData;
     }
+
 
 }
